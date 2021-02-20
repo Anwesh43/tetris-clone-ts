92fb152b0d4e06783833f17665ee6c373861f2d1
Anwesh43 <anweshthecool0@gmail.com>
2021-02-20 14:27:56 +0530

    adding Loop class and object

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const w : number = window.innerWidth 
 const h : number = window.innerHeight  
 const backColor : string = "#BDBDBD"
+const delay : number = 50 
 
 class Stage {
 
@@ -32,3 +33,25 @@ class Stage {
         stage.handleTap()
     }
 }
+
+class Loop {
+
+    animated : boolean = false
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+}
+
+const loop : Loop = new Loop()
